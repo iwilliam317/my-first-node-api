9e268c54fc3de85c43249db4303fdb3539cea21c
william <willshinji@gmail.com>
2018-04-14 01:32:19 -0300

    formats the buffer chunk into a readable content

--- cliente/android.js
+++ cliente/android.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
 http.get('http://localhost:3000', function(res){
+
+  let buffer_body_response = [];
+
   res.on('data', function(chunk){
-    console.log(chunk);
+    buffer_body_response.push(chunk);
   });
 
   res.on('end', function(){
+    /*formats the buffer chunk into a readable content*/
+    let body_response = Buffer.concat(buffer_body_response).toString();
+    console.log(body_response);
   });
-  
+
 });
